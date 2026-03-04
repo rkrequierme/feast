fd0bd76b0b0766d49fe6ead9b24c3ff0e4c15cad
Nathan0721 <nathan.alisangco@gmail.com>
2026-03-04 14:09:56 +0800

    Add more colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
-const Color feastLightGreen = Color(0xFFB4DCCD);
+// Main Colors & Accents
 const Color feastGreen = Color(0xFF2E7D32);
-const Color feastLightYellow = Color(0xFFFFF59D);
+const Color feastLightGreen = Color(0xFFB4DCCD);
 const Color feastBlue = Color(0xFF0277BD);
-const Color feastWarning = Color(0xFFDC143C);
+const Color feastDarkBlue = Color(0xFF01579B);
+const Color feastLightBlue = Color(0xFF81D4FA);
+const Color feastLighterBlue = Color(0xFFB3E5FC);
+const Color feastLightBlueAccent = Color(0xFF90CAF9);
+const Color feastOrange = Color(0xFFF57F17);
+const Color feastLightYellow = Color(0xFFFFF59D);
+const Color feastLighterYellow = Color(0xFFFEF8C4);
+const Color feastLightYellowAccent = Color(0xFFFFE082);
+
+// Neutral Colors
+const Color feastGray = Color(0xFF656070);
 const Color feastBlack = Color(0xFF1D1B20);
+
+// Widget Colors
+const Color feastLogTitle = Color(0xFF00696D);
 const Color feastUnselected = Color(0xFF9BB97D);
 const Color feastNavBarBackground = Color(0xFFE8F5E9);
+
+// Status Colors
 const Color feastLink = Color(0xFF0000FF);
+const Color feastInfo = Color(0xFF0088FF);
+const Color feastWarning = Color(0xFFDC143C);
+const Color feastSuccess = Color(0xFF34C759);
+const Color feastPending = Color(0xFFFF8D28);
+const Color feastError = Color(0xFFFF383C);
